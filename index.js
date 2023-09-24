d95f727106fbd153d0f6ae7e88bd929bf1645d20
Mayank Singh <msingh@cogitate.us>
2023-09-24 14:02:10 +0530

    contact_list app base setup

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
-console.log("Hello World");
+// console.log("Hello World");
 
-function add(a, b) {
-  return a + b;
-}
+// function add(a, b) {
+//   return a + b;
+// }
 
-console.log(process.argv);
+// console.log(process.argv);
 
-var args = process.argv.slice(2);
+// var args = process.argv.slice(2);
 
-console.log("Addition of two arguments:", add(parseInt(args[0]) , parseInt(args[1])));
+// console.log("Addition of two arguments:", add(parseInt(args[0]) , parseInt(args[1])));
 
-module.exports = {
-  add: add
-}
+// module.exports = {
+//   add
+// }
+
+// taking array as input and adding all the numbers in it
+const args = process.argv.slice(2);
+
+console.log(args.reduce());
